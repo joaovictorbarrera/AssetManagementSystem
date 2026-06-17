ff6ec23f7c2586c84f532ba0227971d0c1c79b75
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-06-16 23:20:28 -0400

    Added echo response to / and /api

---
+++ server/Controllers/EchoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThreatlockerAssetManagementSystem.Controllers
+{
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet("/")]
+        [HttpGet("/api")]
+        public IActionResult Echo()
+        {
+            return Ok(new 
+            { 
+                Hostname = Environment.MachineName, 
+                InboundIPAddress = HttpContext.Connection.RemoteIpAddress?.ToString() 
+            });
+        }
+    }
+}
