2c59dd772017a86d4cca674ba8ddb9e0068f1e09
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-06-16 21:50:18 -0400

    prod connection string

---
+++ server/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using ThreatlockerAssetManagementSystem.Data;
+
+public static class DatabaseExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration config)
+    {
+        string? host = Environment.GetEnvironmentVariable("DB_HOST");
+        string? port = Environment.GetEnvironmentVariable("DB_PORT");
+        string? database = Environment.GetEnvironmentVariable("DB_NAME");
+        string? username = Environment.GetEnvironmentVariable("DB_USER");
+        string? password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+        string connectionString = (!string.IsNullOrEmpty(host) &&
+                                   !string.IsNullOrEmpty(port) &&
+                                   !string.IsNullOrEmpty(database) &&
+                                   !string.IsNullOrEmpty(username) &&
+                                   !string.IsNullOrEmpty(password))
+            ? $"Server=tcp:{host},{port};Initial Catalog={database};Persist Security Info=False;User ID={username};Password={password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+            : config.GetConnectionString("DefaultConnection")
+              ?? throw new InvalidOperationException("No database connection string configured.");
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        return services;
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -6,13 +6,9 @@ using ThreatlockerAssetManagementSystem.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-    
-builder.Services.AddCustomCors(builder.Environment);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDatabase(builder.Configuration)
+                .AddCustomCors(builder.Environment);
 
 builder.Services.AddScoped<UserRepository>();
 
