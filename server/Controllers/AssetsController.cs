8e6ec7ed5188c6f164c51460a6c1d8ec14f692b1
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-06-18 21:52:53 -0400

    Created assets and users controllers with all routes (not-implemented)

---
+++ server/Controllers/AssetsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThreatlockerAssetManagementSystem.Controllers
+{
+    [Authorize]
+    [Route("api/assets")]
+    [ApiController]
+    public class AssetsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAssets()
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "AssetManager+")]
+        public async Task<IActionResult> CreateAsset()
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsset(Guid id)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Policy = "AssetManager+")]
+        public async Task<IActionResult> UpdateAsset(Guid id)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> ArchiveAsset(Guid id)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpPatch("{id}/status")]
+        [Authorize(Policy = "AssetManager+")]
+        public async Task<IActionResult> UpdateAssetStatus(Guid id)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpPatch("{id}/condition")]
+        [Authorize(Policy = "AssetManager+")]
+        public async Task<IActionResult> UpdateAssetCondition(Guid id)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpGet("fields")]
+        [Authorize(Policy = "AssetManager+")]
+        public async Task<IActionResult> GetAssetFields()
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpGet("{id}/history")]
+        [Authorize(Policy = "AssetManager+")]
+        public async Task<IActionResult> GetAssetHistory(Guid id)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+    }
+}
---
+++ server/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThreatlockerAssetManagementSystem.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser()
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpPatch("{id}/role")]
+        public async Task<IActionResult> UpdateUserRole(Guid id)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        [HttpPatch("{id}/active")]
+        public async Task<IActionResult> UpdateUserActiveStatus(Guid id)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+    }
+}
