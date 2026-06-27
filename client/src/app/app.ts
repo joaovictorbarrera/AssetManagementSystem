6656c25d694fd33f947a5fd735946fdeb9589870
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-06-27 10:14:19 -0400

    Added loading state pre-auth

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageLoadingService } from './core/services/page-loading.service';
+import { SpinningWheel } from './core/components/spinning-wheel/spinning-wheel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SpinningWheel],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  constructor(readonly pageLoading: PageLoadingService) {}
 }
--- client/src/app/core/guards/auth.guard.ts
+++ client/src/app/core/guards/auth.guard.ts
@@ -1,12 +1,16 @@
 import { inject } from "@angular/core";
 import { CanActivateChildFn, Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
+import { PageLoadingService } from "../services/page-loading.service";
 
 export const authGuard: CanActivateChildFn = async () => {
   const auth = inject(AuthService)
   const router = inject(Router)
+  const pageLoading = inject(PageLoadingService)
 
+  pageLoading.setLoading(true)
   const user = await auth.loadUser()
+  pageLoading.setLoading(false)
 
   return user
     ? true
