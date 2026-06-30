9074a46f2c7d781d49d981336aa34659ca160eec
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-06-30 00:17:53 -0400

    Added logo

--- client/src/app/core/layout/main-layout/navbar/navbar.ts
+++ client/src/app/core/layout/main-layout/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../services/api/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html'
 })
 export class Navbar {
