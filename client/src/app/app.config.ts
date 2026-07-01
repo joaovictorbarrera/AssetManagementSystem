d76bf195a36bfd9ee32f77e83e19b2486f6fd87b
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-06-30 20:31:04 -0400

    added refresh button to all pages

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
 import { provideIcons } from '@ng-icons/core';
-import { heroCalendar, heroCheck, heroChevronLeft, heroChevronRight, heroClipboardDocumentCheck, heroComputerDesktop, heroInboxArrowDown, heroListBullet, heroMagnifyingGlass, heroPlus, heroServerStack, heroShieldCheck, heroUser, heroXMark } from '@ng-icons/heroicons/outline';
+import { heroArrowPath, heroCalendar, heroCheck, heroChevronLeft, heroChevronRight, heroClipboardDocumentCheck, heroComputerDesktop, heroInboxArrowDown, heroListBullet, heroMagnifyingGlass, heroPlus, heroServerStack, heroShieldCheck, heroUser, heroXMark } from '@ng-icons/heroicons/outline';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -29,7 +29,8 @@ export const appConfig: ApplicationConfig = {
       heroClipboardDocumentCheck,
       heroXMark,
       heroCheck,
-      heroCalendar
+      heroCalendar,
+      heroArrowPath
     })
   ]
 }
